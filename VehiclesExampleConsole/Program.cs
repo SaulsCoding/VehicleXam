7f5edc2d59f5cae6a65908e11b97a02674563ed1
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2021-04-28 15:05:08 +0200

    first commit

---
+++ VehiclesExampleConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Vehicles;
+
+namespace VehiclesExampleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car hyundai = new Car();
+
+            hyundai.DriveFaster(10);
+            Console.WriteLine(hyundai.Speed);
+        }
+    }
+}
---
+++ VehiclesMobile/VehiclesMobile/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Vehicles;
+
+namespace VehiclesMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        private Car _mazda323;
+        public MainPage()
+        {
+            
+            InitializeComponent();
+            _mazda323 = new Car();
+            _mazda323.Make = "Mazda";
+
+            CarMakeLabel.Text = _mazda323.Make;
+            
+        }
+
+        private void DriveFasterButton_Clicked(object sender, EventArgs e)
+        {
+            _mazda323.DriveFaster(50);
+            CarSpeedLabel.Text = _mazda323.Speed.ToString();//Speed property that is an integer
+        }
+
+        
+    }
+}
